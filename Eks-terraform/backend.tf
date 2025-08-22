f0582f7e9c3687a2f1b575a6dc7de5c227ce556a
vikicr7 <vikiwayne3437@gmail.com>
2025-08-22 15:13:51 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "newk8svjbuck" # Replace with your actual S3 bucket name
+    bucket = "vikibjkjdfjkjdf" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
